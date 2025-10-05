0b1a6408257cf1ee5cda2293e56e377f29a3a4a0
Joakim Nøddeskov Clifford <joacl@dtu.dk>
2025-10-05 22:58:37 +0200

    added some installation files

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bepipocket',
+    version='0.1',
+    packages=find_packages(),  
+    include_package_data=True,  # Include non-code files specified in MANIFEST.in
+    package_data={
+        'bepipocket': [
+            'models/hmm_antibody_identification/*.hmm' # include hmm models for antibody identification
+        ]
+    },
+    install_requires=[
+        # List your package's dependencies here
+    ],
+    author='Joakim Clifford',
+    author_email='cliffordjoakim@gmail.com',
+    description='BepiPocket: Predict antibody-antigen structures using Chai-1 guided by antibody-epitope restraints predicted by BepiPred-3.0 or DiscoTope-3.0.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/mnielLab/BepiPocket-1.0.git',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
